9db5ad0e5673e9496ae89e2e908058b76bdc23f6
TimHuijbens <tim.huijbens@hotmail.com>
2025-05-27 11:18:23 +0200

    Initial commit from Spring Initializr

---
+++ src/test/java/com/example/moviefinder/MoviefinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviefinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
